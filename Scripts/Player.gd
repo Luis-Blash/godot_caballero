1533187eedf0635215a12ff8d164941d74623556
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2023-10-29 17:36:54 -0600

    feat: move person in horizontal

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
  
 @onready var _animation_player = $AnimationPlayer
 
+@export var speed : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,4 +11,10 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(_delta):
+	motion_ctrl()
 	pass
+	
+func motion_ctrl() -> void: 
+	velocity.x = Global.get_axis().x * speed
+	#velocity.y = Global.get_axis().y * speed
+	move_and_slide()
--- Scripts/global.gd
+++ Scripts/global.gd
@@ -1,12 +1,12 @@
 extends Node
 
+var score : int
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print("hola global")
-	pass # Replace with function body.
+# Movimiento del player, ejes de movimiento
+var axis : Vector2
 
+func get_axis() -> Vector2:
+	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	axis.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	return axis.normalized()	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
