7893a3ab0cad0b4c9793c50b242d8b2bc679d9fc
Luis <blash3g@gmail.com>
2023-11-25 20:30:37 -0600

    feat: jump Person

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,7 +5,9 @@ extends CharacterBody2D
 @onready var _colisionBody = $CollisionShape2D
 
 @export var speed : float = 200
-@export var mass : float = 2
+@export var mass : float = 3
+var is_jumping: bool = false
+@export var jump_speed: float = 400
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,18 +19,34 @@ func _process(_delta):
 	pass
 
 func _physics_process(delta):
-	gravity_character()
 	motion_ctrl()
-	move_and_slide()
 	select_animation()
-
-func gravity_character():
-	velocity.y = Global.get_gravity_character(velocity.y, mass)
+	move_and_slide()
 
 func motion_ctrl() -> void: 
 	velocity.x = Global.get_axis().x * speed
 	
+	# si esta tocando el suelo puedes saltar
+	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
+		velocity.y = -jump_speed
+		is_jumping = true
+	
+	# Si esta saltando
+	if is_jumping:
+		is_jumping = false
+	# si no salta le afecta la gravedad
+	else:
+		velocity.y = Global.get_gravity_character(velocity.y, mass)
+		
+	
 func select_animation():
+	
+	#if(is_on_floor()):
+	#	print("esta tocando el suelo")
+	#else: 
+	#	print("esta en el cielo")
+	
+	
 	if(Input.is_action_pressed("ui_left") || Input.is_action_pressed("ui_right")):
 		if(Input.is_action_pressed("ui_left")):
 			_sprite_player.flip_h = true
@@ -40,5 +58,3 @@ func select_animation():
 			_animation_player.play("run_player")
 	else :
 		_animation_player.play("idle_player")
-	#_animation_player.play("idle_player")
-	pass
