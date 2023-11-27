12a5757ef338d91fc36dad00bdda38889eaac653
Luis <blash3g@gmail.com>
2023-11-26 20:01:51 -0600

    feat: changue logic for more understand@

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,11 +3,16 @@ extends CharacterBody2D
 @onready var _animation_player = $AnimationPlayer
 @onready var _sprite_player = $Sprite2D
 @onready var _colisionBody = $CollisionShape2D
+@onready var _wallRaycast = $WallRayCast
 
 @export var speed : float = 200
 @export var mass : float = 3
 var is_jumping: bool = false
 @export var jump_speed: float = 400
+
+var jump_key = false
+var left_key = false
+var right_key = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,21 +20,41 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(_delta):
-	
 	pass
 
 func _physics_process(delta):
+	keys_press()
 	motion_ctrl()
 	select_animation()
 	move_and_slide()
+	
+func keys_press():
+	
+	if Input.is_action_just_pressed("ui_up"):
+		jump_key = true
+	else:
+		jump_key = false
+		
+	if Input.is_action_pressed("ui_left"):
+		left_key = true
+	else:
+		left_key = false
+		
+	if Input.is_action_pressed("ui_right"):
+		right_key = true
+	else:
+		right_key = false
+		
+
 
 func motion_ctrl() -> void: 
 	velocity.x = Global.get_axis().x * speed
 	
 	# si esta tocando el suelo puedes saltar
-	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
-		velocity.y = -jump_speed
-		is_jumping = true
+	if is_on_floor() and jump_key:
+		if !get_collision_wall():
+			velocity.y = -jump_speed
+			is_jumping = true
 	
 	# Si esta saltando
 	if is_jumping:
@@ -39,22 +64,20 @@ func motion_ctrl() -> void:
 		velocity.y = Global.get_gravity_character(velocity.y, mass)
 		
 	
-func select_animation():
+func select_animation():	
 	
-	#if(is_on_floor()):
-	#	print("esta tocando el suelo")
-	#else: 
-	#	print("esta en el cielo")
-	
-	
-	if(Input.is_action_pressed("ui_left") || Input.is_action_pressed("ui_right")):
-		if(Input.is_action_pressed("ui_left")):
-			_sprite_player.flip_h = true
-			_colisionBody.transform[2] = Vector2(5,22)
-			_animation_player.play("run_player")
-		else: 
-			_sprite_player.flip_h = false
-			_colisionBody.transform[2] = Vector2(-5,22)
-			_animation_player.play("run_player")
-	else :
+	if (left_key || right_key):
+		_animation_player.play("run_player")
+	else:
 		_animation_player.play("idle_player")
+		
+	if left_key:
+		_sprite_player.flip_h = true
+		_colisionBody.transform[2] = Vector2(5,22)
+	
+	if right_key:
+		_sprite_player.flip_h = false
+		_colisionBody.transform[2] = Vector2(-5,22)
+
+func get_collision_wall() -> bool:
+	return _wallRaycast.is_colliding()
