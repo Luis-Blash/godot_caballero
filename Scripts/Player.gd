4d74ed71135c9cf3cf0bcd544e6164d8eb78058d
Luis <blash3g@gmail.com>
2023-12-03 21:30:52 -0600

    feat: animation attack player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -14,6 +14,9 @@ var is_jumping: bool = false
 var jump_key = false
 var left_key = false
 var right_key = false
+var attack_key = false
+
+var select_animation_attack = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -45,7 +48,9 @@ func keys_press():
 		right_key = true
 	else:
 		right_key = false
-		
+	
+	if Input.is_action_pressed("ui_letter_attack") && get_collision_floor():
+		attack_key = true
 
 
 func motion_ctrl() -> void: 
@@ -53,11 +58,11 @@ func motion_ctrl() -> void:
 	if get_collision_floor():
 		velocity = Vector2.ZERO
 	
-	if !get_collision_wall():
+	if !get_collision_wall() && !attack_key:
 		velocity.x = Global.get_axis().x * speed
 	
 	# si esta tocando el suelo puedes saltar
-	if get_collision_floor() and jump_key:
+	if get_collision_floor() && jump_key && !attack_key:
 		if !get_collision_wall():
 			velocity.y = -jump_speed
 			is_jumping = true
@@ -71,13 +76,20 @@ func motion_ctrl() -> void:
 		
 	
 func select_animation():	
-
-	if ((left_key || right_key) && get_collision_floor()):
+	
+	if ((left_key || right_key) && get_collision_floor() && !attack_key):
 		_animation_player.play("run_player")
+		# ataque
 	elif !get_collision_floor():
 		_animation_player.play("Jump_player")
 	else:
-		_animation_player.play("idle_player")
+		if attack_key:
+			if select_animation_attack == 0:
+				_animation_player.play("attack_one")
+			else:
+				_animation_player.play("attack_two")
+		else:
+			_animation_player.play("idle_player")
 		
 	if left_key:
 		_sprite_player.flip_h = true
@@ -99,3 +111,17 @@ func get_collision_wall() -> bool:
 	
 func get_collision_floor() -> bool:
 	return _floorRaycast.is_colliding()
+
+
+func _on_animation_player_animation_started(anim_name):
+	if(anim_name == "atack_one"):
+		pass
+	pass # Replace with function body.
+
+func _on_animation_player_animation_finished(anim_name):
+	if(anim_name == "attack_one" || anim_name == "attack_two"):
+		attack_key = false
+		if anim_name == "attack_one":
+			select_animation_attack = 1
+		else:
+			select_animation_attack = 0
